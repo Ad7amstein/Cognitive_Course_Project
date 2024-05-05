539de878f60955f235b2b2d7788a2e6cd59c9649
Ad7amstein <adham32003200@gmail.com>
2024-05-05 11:11:21 +0300

    Fix main

--- game_ai/tetris_base.py
+++ game_ai/tetris_base.py
@@ -173,14 +173,15 @@ MANUAL_GAME = True
 ##############################################################################
 # MAIN GAME
 ##############################################################################
-global FPSCLOCK, DISPLAYSURF, BASICFONT, BIGFONT
-pygame.init()
+def main():
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, BIGFONT
+    pygame.init()
 
-FPSCLOCK    = pygame.time.Clock()
-DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-BASICFONT   = pygame.font.Font('freesansbold.ttf', 18)
-BIGFONT     = pygame.font.Font('freesansbold.ttf', 100)
-pygame.display.set_caption('Tetris AI')
+    FPSCLOCK    = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    BASICFONT   = pygame.font.Font('freesansbold.ttf', 18)
+    BIGFONT     = pygame.font.Font('freesansbold.ttf', 100)
+    pygame.display.set_caption('Tetris AI')
 
 def run_game():
     # Setup variables
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from game_ai.tetris_base import *
+from game_ai.tetris_base import main
+from game_ai.tetris_base import MANUAL_GAME
+from game_ai.tetris_base import run_game
 from game_ai.algorithm import run_game_ai
 
 ##############################################################################
 # MAIN GAME
 ##############################################################################
 def main():
+    main()
     if (MANUAL_GAME):
         run_game()
     else:
